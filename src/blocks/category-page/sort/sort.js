89ce939cec8476cf3fd1faf87ea96817bfef18a6
hamst <hamsterart.it@gmail.com>
2023-03-31 01:43:49 +0600

    add

--- src/blocks/category-page/sort/sort.js
+++ src/blocks/category-page/sort/sort.js
@@ -2,17 +2,21 @@ const sortItems = function () {
   const box = document.querySelector('[data-sort-box]');
   const btn = document.querySelector('[data-sort-btn]');
 
-  btn.addEventListener('click', function () {
-    box.classList.toggle('_active');
-  });
+  if(btn) {
+    btn.addEventListener('click', function () {
+      box.classList.toggle('_active');
+    });
+  }
 
   const filterBtn = document.querySelector('[data-mobile-filter-btn]');
   const filterDropdown = document.querySelector('[data-filter-box]');
 
-  filterBtn.addEventListener('click', function () {
-    filterBtn.classList.toggle('filter-mobile-btn--active');
-    filterDropdown.classList.toggle('category-wrapper__sidebar--active');
-  });
+  if(filterBtn) {
+    filterBtn.addEventListener('click', function () {
+      filterBtn.classList.toggle('filter-mobile-btn--active');
+      filterDropdown.classList.toggle('category-wrapper__sidebar--active');
+    });
+  }
 };
 
 export default sortItems;
